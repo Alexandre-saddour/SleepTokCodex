dfc0922f4c8c989d2d76b4c49e07167871392b5a
alex <alex.saddour@gmail.com>
2026-01-23 17:37:57 +0100

    Add app package structure

---
+++ composeApp/src/commonMain/kotlin/com/example/kmpbackbone/resources/AppResources.kt
@@ -0,0 +1,3 @@
+package com.example.kmpbackbone.resources
+
+object AppResources
--- composeApp/src/commonMain/kotlin/com/example/kmpbackbone/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmpbackbone/ui/home/HomeScreen.kt
@@ -1,4 +1,4 @@
-package com.example.kmpbackbone.home
+package com.example.kmpbackbone.ui.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
